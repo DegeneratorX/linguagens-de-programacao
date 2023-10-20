220a62b97d15b5b39ae0cceb2c37302e8243a415
Victor Martins <victorio.deg@gmail.com>
2023-10-20 19:55:40 -0300

    Ola

--- Trab1/python.py
+++ Trab1/python.py
@@ -1,74 +1,107 @@
-# Definição do nó da árvore
-class TreeNode:
-    def __init__(self, key, val, left=None, right=None):
-        self.key = key
-        self.val = val
-        self.left = left
-        self.right = right
-        self.x = 0
-        self.y = 0
+# Global variable for scale
+SCALE = 30
 
-# Função de auxílio para adicionar coordenadas X, Y a cada nó
-def add_xy(tree):
-    if tree is None:
-        return None
-    
-    new_tree = TreeNode(tree.key, tree.val, add_xy(tree.left), add_xy(tree.right))
-    return new_tree
+def depth_first(tree, level, left_lim, root_x=None, right_lim=None):
+    if isinstance(tree, dict):
+        x = tree.get('x')
+        y = tree.get('y')
+        left = tree.get('left')
+        right = tree.get('right')
 
-# Algoritmo de desenho da árvore (percorrendo em profundidade)
-def depth_first(tree, level=1, left_lim=0, scale=30):
-    if tree is None:
-        return left_lim, left_lim
+        if left == 'leaf' and right == 'leaf':
+            return (root_x, SCALE * level)
+        elif left != 'leaf' and right == 'leaf':
+            return depth_first(left, level+1, left_lim, root_x, right_lim)
+        elif left == 'leaf' and right != 'leaf':
+            return depth_first(right, level+1, left_lim, root_x, right_lim)
+        else:
+            l_root_x, l_right_lim = depth_first(left, level+1, left_lim, root_x, right_lim)
+            r_left_lim = l_right_lim + SCALE
+            r_root_x, r_right_lim = depth_first(right, level+1, r_left_lim, root_x, right_lim)
+            root_x = (l_root_x + r_root_x) // 2
+            return (root_x, SCALE * level)
+    return None
 
-    if tree.left is None and tree.right is None:
-        tree.x = left_lim
-        tree.y = scale * level
-        return left_lim, left_lim
+def prog(s):
+    s1, s2 = s.split(' ', 1)
+    y = id_fn(s2)
+    s3 = s2.split(';', 1)[1]
+    z = stat(s3)
+    sn = s3.split('end', 1)[1]
+    return ('prog', y, z)
 
-    if tree.left is not None and tree.right is None:
-        right_lim = depth_first(tree.left, level + 1, left_lim, scale)[1]
-        tree.x = (left_lim + right_lim) // 2
-        tree.y = scale * level
-        return left_lim, right_lim
+def stat(s):
+    t, s2 = s.split(' ', 1)
+    if t == 'begin':
+        return sequence(stat, lambda x: x == ';', s2, 'end')
+    elif t == 'if':
+        c, s3 = comp(s2)
+        x1 = stat(s3.split('then', 1)[1])
+        x2 = stat(s3.split('else', 1)[1])
+        return ('if', c, x1, x2)
+    elif t == 'while':
+        c, s3 = comp(s2)
+        x = stat(s3.split('do', 1)[1])
+        return ('while', c, x)
+    elif t == 'read':
+        return ('read', id_fn(s2))
+    elif t == 'write':
+        return ('write', expr(s2))
+    elif is_ident(t):
+        e = expr(s2.split(':=', 1)[1])
+        return ('assign', t, e)
+    else:
+        raise Exception("Error")
 
-    if tree.left is None and tree.right is not None:
-        right_lim = depth_first(tree.right, level + 1, left_lim, scale)[1]
-        tree.x = (left_lim + right_lim) // 2
-        tree.y = scale * level
-        return left_lim, right_lim
+def sequence(non_term, sep, s1, end_token):
+    x1, rest = non_term(s1)
+    if sep(rest.split(' ')[0]):
+        x2 = sequence(non_term, sep, rest.split(' ', 1)[1], end_token)
+        return (x1, x2)
+    else:
+        return x1
 
-    l_root_x, l_right_lim = depth_first(tree.left, level + 1, left_lim, scale)
-    r_left_lim = l_right_lim + scale
-    r_root_x, right_lim = depth_first(tree.right, level + 1, r_left_lim, scale)
+def comp(s):
+    return sequence(expr, cop, s, None)
 
-    tree.x = (l_root_x + r_root_x) // 2
-    tree.y = scale * level
-    return l_root_x, right_lim
+def expr(s):
+    return sequence(term, eop, s, None)
 
-# Testando com um exemplo de árvore
-if __name__ == "__main__":
-    # Construindo a árvore exemplo
-    tree = TreeNode('a', 111,
-                    TreeNode('b', 55, 
-                             TreeNode('x', 100, 
-                                      TreeNode('z', 56), 
-                                      TreeNode('w', 23)),
-                             TreeNode('y', 105, None,
-                                      TreeNode('r', 77))),
-                    TreeNode('c', 123,
-                             TreeNode('d', 119,
-                                      TreeNode('g', 44),
-                                      TreeNode('h', 50,
-                                               TreeNode('i', 5),
-                                               TreeNode('j', 6))),
-                             TreeNode('e', 133)))
+def term(s):
+    return sequence(fact, top, s, None)
 
-    # Adicionando coordenadas X e Y
-    tree = add_xy(tree)
+def cop(y):
+    return y in ['<', '>', '<=', '>=', '==', '!=']
 
-    # Aplicando o algoritmo de desenho de árvore
-    depth_first(tree)
+def eop(y):
+    return y in ['+', '-']
 
-    # Printando as coordenadas dos nós como um teste
-    print(tree.x, tree.y)  # Saída deveria ser as coordenadas x, y do nó raiz
+def top(y):
+    return y in ['*', '/']
+
+def fact(s):
+    if is_int(s.split(' ')[0]) or is_ident(s.split(' ')[0]):
+        return s.split(' ')[0], s.split(' ', 1)[1]
+    else:
+        e = expr(s.split('(', 1)[1].split(')', 1)[0])
+        return e
+
+def id_fn(s):
+    x = s.split(' ')[0]
+    if is_ident(x):
+        return x, s.split(' ', 1)[1]
+    return None
+
+def is_ident(x):
+    return isinstance(x, str) and x.isalpha()
+
+def is_int(t):
+    try:
+        int(t)
+        return True
+    except ValueError:
+        return False
+
+# Main execution
+a = prog("program foo ; while a + 3 < b do b := b + 1 end")
+print(a)
